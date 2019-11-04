5de29f913cc89d562f64595dd4b3a2110a57b58a
Grigorii Horos <horosgrisa@gmail.com>
2019-11-04 12:03:17 +0200

    Fix

--- pr-php.plugin.zsh
+++ pr-php.plugin.zsh
@@ -6,14 +6,13 @@ PHP_PREFIX=${PHP_PREFIX:-" "}
 PHP_SUFIX=${PHP_SUFIX:-""}
 
 
-if command -v zpm >/dev/null; then
+if (( $+function[zpm] )); then
   zpm zpm-zsh/helpers zpm-zsh/colors
 fi
 
 pr_php=""
 
 _pr_php() {
-  
   if (( $+commands[php] )); then
     if is-recursive-exist composer.json >/dev/null \
     || is-recursive-exist index.php >/dev/null ; then
@@ -23,14 +22,13 @@ _pr_php() {
         php --version | sed 1q | awk '{print $2}' | awk -F'-' '{print $1}'
       )
       pr_php+="%{$c[magenta]${c_bold}%}𝗛%{$c_reset%} %{$c[blue]$c_bold%}$php_version%{$c_reset%}"
-      
       pr_php+="$PHP_SUFIX"
+      
       return 0
     fi
   fi
   
   pr_php=""
-  
 }
 
 _pr_php
