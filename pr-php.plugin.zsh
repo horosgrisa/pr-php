3043aeafe6da86e0b2dba8d3eb2c14ba1e405948
Grigorii Horos <horosgrisa@gmail.com>
2023-08-18 00:04:42 +0300

    Change ui

--- pr-php.plugin.zsh
+++ pr-php.plugin.zsh
@@ -1,17 +1,24 @@
 #!/usr/bin/env zsh
 
-typeset -g pr_php=""
+: ${PR_PHP_SYMBOL:=''}
+
+typeset -g pr_php
 
 if (( $+commands[php] )); then
   function _pr_php() {
-    pr_php=""
+    pr_php=''
 
     if is-recursive-exist composer.json index.php; then
+      pr_php=" %{${c[base3]}${c[bold]}%}(%{${c[reset]}%}"
+
       local -a lines=( ${(f)"$(command php --version)"} )
       local -a arr_mod=("${(@s/ /)lines[1]}")
-      local php_version=$arr_mod[2]
+      local phpver=$arr_mod[2]
 
-      pr_php=" %{${c[magenta]}${c[bold]}%}PHP%{${c[yellow]}%}:%{${c[blue]}%}$php_version%{${c[reset]}%}"
+      pr_php+="%{${c[violet]}${c[bold]}%}${PR_PHP_SYMBOL}%{${c[reset]}%} %{${c[violet]}${c[bold]}%}$phpver%{${c[reset]}%}"
+
+      pr_php+="%{${c[base3]}${c[bold]}%})%{${c[reset]}%}"
+
     fi
   }
 
